8b5bed52a65006c21965775e54253349f6f9e705
pedrolucaslcosta <pedrolucaslcosta@gmail.com>
2022-12-19 20:55:38 -0300

    1.2.6 - Minor code fixes at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // JS
 import "bootstrap/js/dist/collapse.js";
 import "bootstrap/js/dist/offcanvas.js";
+import "bootstrap/js/dist/modal.js";
 
 // Components
 import { Task } from "./components/Task";
@@ -16,14 +17,22 @@ import { AddTaskForm } from "./components/AddTaskForm";
 
 // Font Awesome Icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHamburger, faInbox, faPlus, faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHamburger,
+  faInbox,
+  faPlus,
+  faBars,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 // ---------- App -----------------------------------
 
 function App() {
   // Tasks State
   const [todoList, setTodoList] = useState([
-    { id: Math.random().toString(16).slice(2), name: "Buy milk", status: true },
+    { id: Math.random().toString(16).slice(2), 
+      name: "Buy milk",
+      status: true },
     {
       id: Math.random().toString(16).slice(2),
       name: "Drink water",
@@ -123,128 +132,12 @@ function App() {
   // App Return
   return (
     <div>
-
-      {/* <nav className="navbar navbar-expand-lg bg-dark border-bottom border-secondary">
-        <div className="container-fluid">
-        <button className="btn bg-transparent text-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><FontAwesomeIcon icon={faBars} /></button>
-        <form className="d-flex" role="search">
-          <div className="input-group input-group-sm mb-3">
-            <input type="text" className="form-control bg-dark text-light" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
-            <span className="input-group-text bg-dark text-light" id="inputGroup-sizing-sm"><FontAwesomeIcon icon={faMagnifyingGlass} /></span>
-          </div>    
-        </form>
-          <a className="navbar-brand" href="#"><FontAwesomeIcon icon={faInbox} /> To-do List</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbar">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            
-          </div>
-        </div>
-      </nav>
-
-      <div className="d-flex nowrap">
-
-      
-
-      <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
-          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-          <p>Try scrolling the rest of the page to see this option in action.</p>
-        </div>
-      </div>
-
-      <div className="d-flex flex-column flex-shrink-0 p-3 border-end border-secondary offcanvas vh-100">
-          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span className="fs-4">Sidebar</span>
-          </a>
-          <hr/>
-          <ul className="nav nav-pills flex-column mb-auto">
-            <li className="nav-item">
-              <a href="#" className="nav-link active" aria-current="page">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link text-white">
-                
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link text-white">
-                
-                Orders
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link text-white">
-                
-                Products
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link text-white">
-                
-                Customers
-              </a>
-            </li>
-          </ul>
-          <hr/>
-          <div className="dropdown">
-            <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2"/>
-              <strong>mdo</strong>
-            </a>
-            <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-              <li><a className="dropdown-item" href="#">New project...</a></li>
-              <li><a className="dropdown-item" href="#">Settings</a></li>
-              <li><a className="dropdown-item" href="#">Profile</a></li>
-              <li><hr className="dropdown-divider"/></li>
-              <li><a className="dropdown-item" href="#">Sign out</a></li>
-            </ul>
-          </div>
-        </div> */}
-
-
       <div className="container App p-0 mt-3 mb-5">
-        <div className="card col-lg-6 col-md-10 col-11 col-sm-11 border-secondary bg-dark text-light mx-auto">
+        <div className="card col-lg-6 col-md-10 col-12 col-sm-12 border-secondary bg-dark text-light mx-auto">
           <div className="card-header border-secondary p-3 d-flex justify-content-between align-middle">
             <h5 className="card-title">
               <FontAwesomeIcon icon={faInbox} /> To-do List
             </h5>
-            {/* <button
-              className="btn btn-primary fw-semibold text-light btn-sm px-4 ms-3"
-              onClick={handleVisibility}
-              type="button"
-            >
-              <FontAwesomeIcon icon={faPlus} />
-            </button> */}
             <button
               className="btn btn-outline-secondary fw-semibold text-light position-fixed bottom-0 end-0 mb-3 me-3"
               onClick={handleVisibility}
@@ -255,42 +148,54 @@ function App() {
           </div>
           <div className="card-body">
             <div className="">
-              {visibility && (
-                <AddTaskForm
-                  newTask={newTask}
-                  handleNewTask={handleNewTask}
-                  handleVisibility={handleVisibility}
-                  addTask={addTask}
-                  cancelAdd={cancelAdd}
-                />
-              )}
-            </div>
-
-            <ul className="list-group mt-3">
-              {todoList && todoList.length ? "" : "No tasks..."}
-
-              {todoList
-
-                // Display tasks
-                .map((task) => {
-                  return (
-                    <Task
-                      id={task.id}
-                      name={task.name}
-                      status={task.status}
-                      completeTask={completeTask}
-                      deleteTask={deleteTask}
-                      handleUpdateTask={handleUpdateTask}
-                      setUpdateData={setUpdateData}
-                      updateData={updateData}
-                      changeTask={changeTask}
-                      updateTask={updateTask}
-                      cancelUpdate={cancelUpdate}
-                      key={task.id}
+              <div className="">
+                <div className="">
+                  {visibility && (
+                    <AddTaskForm
+                      newTask={newTask}
+                      handleNewTask={handleNewTask}
+                      handleVisibility={handleVisibility}
+                      addTask={addTask}
+                      cancelAdd={cancelAdd}
                     />
-                  );
-                })}
-            </ul>
+                  )}
+                </div>
+                <ul className="list-group mt-3">
+                  {/* <li className="list-group-item list-group-item-action border-0 bg-dark border-secondary text-light d-flex justify-content-between align-items-center">
+                    <a
+                      className="text-secondary text-decoration-none"
+                      onClick={handleVisibility}
+                      type="button"
+                    >
+                      <FontAwesomeIcon icon={faPlus} /> Add Task
+                    </a>
+                  </li> */}
+                  {todoList && todoList.length ? "" : "No tasks..."}
+
+                  {todoList
+
+                    // Display tasks
+                    .map((task) => {
+                      return (
+                        <Task
+                          id={task.id}
+                          name={task.name}
+                          status={task.status}
+                          completeTask={completeTask}
+                          deleteTask={deleteTask}
+                          handleUpdateTask={handleUpdateTask}
+                          setUpdateData={setUpdateData}
+                          updateData={updateData}
+                          changeTask={changeTask}
+                          updateTask={updateTask}
+                          cancelUpdate={cancelUpdate}
+                          key={task.id}
+                        />
+                      );
+                    })}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
